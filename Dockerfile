124da7850ba0e20f67a37eaf070e8facbc6efd4b
Mateusz Juda <mateusz.juda@{ii.uj.edu.pl,gmail.com}>
2019-07-18 15:59:16 +0200

    verbose download

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,6 @@ LABEL CAPD_VERSION=${CAPD_VERSION}
 RUN mkdir /tmp/capd/ /workdir
 WORKDIR "/tmp/capd"
 
-
-RUN curl -L https://sourceforge.net/projects/capd/files/${CAPD_VERSION}/bin/capd-${CAPD_VERSION}-dev-ubuntu_x86-x86_64.tar.gz/download -o capd-${CAPD_VERSION}-dev-ubuntu_x86-x86_64.tar.gz && curl -L http://svn.capdnet.ii.uj.edu.pl/capdRedHom/examples/apiRedHom/CubicalBettiNumbers.cpp -o CubicalBettiNumbers.cpp
-
 ADD install.sh .
 RUN ./install.sh
 
--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 set -e
+set -x
+
+echo "CAPD_VERSION: ${CAPD_VERSION}"
+
+curl -L https://sourceforge.net/projects/capd/files/${CAPD_VERSION}/bin/capd-${CAPD_VERSION}-dev-ubuntu_x86-x86_64.tar.gz/download -o capd-${CAPD_VERSION}-dev-ubuntu_x86-x86_64.tar.gz
+curl -L http://svn.capdnet.ii.uj.edu.pl/capdRedHom/examples/apiRedHom/CubicalBettiNumbers.cpp -o CubicalBettiNumbers.cpp
 
 tar xzf capd-${CAPD_VERSION}-dev-ubuntu_x86-x86_64.tar.gz  --strip-components=1 -C /
 echo /usr/local/lib >> /etc/ld.so.conf.d/local.conf
