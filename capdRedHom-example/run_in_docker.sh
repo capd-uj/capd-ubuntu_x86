db50c9203a36ad109c491fd42c1febefbc3890ff
Mateusz Juda <mateusz.juda@{ii.uj.edu.pl,gmail.com}>
2019-08-01 11:49:58 +0200

    ver

--- capdRedHom-example/run_in_docker.sh
+++ capdRedHom-example/run_in_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CAPD_VERSION=5.1.0
+CAPD_VERSION=5.1.1
 IMAGE=capd/capd-ubuntu_x86:${CAPD_VERSION}
 
 docker run --rm -ti  --volume $PWD:$PWD --workdir $PWD --user $(id -u):$(id -g) $IMAGE $*
